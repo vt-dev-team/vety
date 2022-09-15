93978a2d7f183e9c5b05fc5c0dd73b4d0f2ccb29
Yemaster <30290923+yemaster@users.noreply.github.com>
2022-09-15 20:55:20 +0800

    Update 1.3.0

--- main/vety.js
+++ main/vety.js
@@ -16,6 +16,22 @@ let Vety = new Vue({
             "Nothing brave, nothing have.", "No one can call back yesterday.", "No rose without a thorn.", "Not to advance is to go back.", "Nothing in the world is difficult for one who sets his mind to it.", "No way is impossible to courage.", "No man can do two things at once.", "No man is born wise or learned.", "No man is content.", "No man is wise at all times.", "None are so blind as those who won’t see.", "None are so deaf as those who won’t hear.", "No news is good news.", "No cross, no crown.", "No pains, no gains.", "No pleasure without pain.", "New wine in old bottles.", "No sweet without sweat.", "No smoke without fire.", "Never too old to learn, never too late to turn.", "Nothing dries sooner than a tear.", "No garden without its weeds.", "Nothing is difficult to the man who will try.", "Nothing seek, nothing find.", "Nothing is so necessary for travelers as languages.", "Nothing is to be got without pains but poverty.", "Never say die.", "Not to know what happened before one was born is always to be a child.", "No living man all things can.", "Obedience is the first duty of a soldier.", "Observation is the best teacher.", "Offense is the best defense.", "Old friends and old wines are best.", "Old sin makes new shame.", "Once a man and twice a child.", "Once a thief, always a thief.", "Once bitten, twice shy.", "One boy is a boy, two boys half a boy, three boys no boy.", "One cannot put back the clock.", "One eyewitness is better than ten hearsays.", "One false move may lose the game.", "One good turn deserves another.", "One hour today is worth two tomorrow.", "One man’s fault is other man’s lesson.", "One never loses anything by politeness.", "One swallow does not make a summer.", "One’s words reflect one’s thinking.", "Out of debt, out of danger.", "Out of office, out of danger.", "Out of sight, out of mind.", "Patience is the best remedy.", "Penny wise, pound foolish.", "Plain dealing is praised more than practiced.", "Please the eye and plague the heart.", "Pleasure comes through toil.", "Pour water into a sieve.", "Practice makes perfect.", "Praise is not pudding.", "Praise makes good men better, and bad men worse.", "Prefer loss to unjust gain.", "Prevention is better than cure.", "Pride goes before, and shame comes after.", "Promise is debt.", "Proverbs are the daughters of daily experience.", "Pull the chestnut out of fire.", "Put the cart before the horse.", "Put your shoulder to the wheel.", "Reading enriches the mind.", "Reading is to the mind while exercise to the body.", "Respect yourself, or no one else will respect you.", "Rome is not built in a day.", "Saying is one thing and doing another.", "Seeing is believing.", "Seek the truth from facts.", "Send a wise man on an errand, and say nothing to him.", "Set a thief to catch a thief.", "He is not fit to command others that cannot command himself.", "Something is better than nothing.", "He is wise that is honest.", "Soon ripe, soon rotten.", "Speech is silver, silence is gold.", "Still water run deep.", "Strike the iron while it is hot.", "Success belongs to the persevering.", "Take things as they come.", "Talking mends no holes.", "Talk of the devil and he will appear.", "Hasty love, soon cold.", "Health is better than wealth.", "Health is happiness.", "Hear all parties.", "He knows most who speaks least", "He is a fool that forgets himself.", "He is a good friend that speaks well of us behind our backs.", "He is a wise man who speaks little.", "He is lifeless that is faultless.", "Short accounts make long friends.", "He is not laughed at that laughs at himself first.", "Soon learn, soon forgotten.", "Heaven never helps the man who will not act."
         ],
         unibody: "window",
+        menu: {
+            tabMenuItems: [{
+                icon: "home alternate",
+                page: "MainContent"
+            }, {
+                icon: "headphones alternate ",
+                page: "ResContent"
+            }, {
+                icon: "cog",
+                page: "Settings"
+            }, {
+                icon: "info alternate",
+                page: "About"
+            }, ],
+            chosenTab: "MainContent",
+        },
         infos: {
             recentFiles: []
         },
@@ -41,10 +57,10 @@ let Vety = new Vue({
             raw: []
         },
         player: {
-            disabled: false
+            disabled: false,
+            nowTime: 0,
+            allTime: 0,
         },
-        nowTime: 0,
-        allTime: 0,
         status: 0,
         playerPercent: 0,
         chooseEle: 0,
@@ -68,10 +84,10 @@ let Vety = new Vue({
         })
         audioElement = document.getElementById("mainMusic")
         audioElement.addEventListener("timeupdate", () => {
-                _t.nowTime = audioElement.currentTime
+                _t.player.nowTime = audioElement.currentTime
                 _t.playerPercent = _t.nowPercent()
-                if (_t.allTime != audioElement.duration) {
-                    _t.allTime = audioElement.duration
+                if (_t.player.allTime != audioElement.duration) {
+                    _t.player.allTime = audioElement.duration
                 } else {
                     _t.$refs.playerSlider.changePercent(_t.playerPercent)
                 }
@@ -85,17 +101,14 @@ let Vety = new Vue({
         $('.ui.accordion')
             .accordion()
         $('.checkbox').checkbox()
-        $('.tabButton, .menu .item')
-            .tab();
+            //$('.tabButton, .menu .item')
+            //    .tab();
         $('.vmenu .menuScroller').css({
             top: `${$(".vmenu a.active.item")[0].offsetTop+12.5}px`,
             height: "15px"
         })
-        $('.vmenu a.item').click((e) => {
-            let p = e.currentTarget
-            $('.vmenu .menuScroller').css('top', `${p.offsetTop+12.5}px`)
-        })
         $(".ui.dropdown.button").dropdown()
+        $(_t.$refs[this.menu.chosenTab]).transition('fade up', '100ms')
         audioElement.addEventListener("play", () => {
             _t.status = 1
         })
@@ -108,6 +121,17 @@ let Vety = new Vue({
     methods: {
         chatWithPyQt: function(c) {
             window.vetyBridge.value = c
+        },
+        changeTab: function(d) {
+            let _t = this
+            if (_t.menu.tabMenuItems[d].page != this.menu.chosenTab) {
+                $('.vmenu .menuScroller').css('top', `${d*40+12.5}px`)
+                $(_t.$refs[this.menu.chosenTab]).transition('fade up', '100ms')
+                _t.menu.chosenTab = _t.menu.tabMenuItems[d].page
+                setTimeout(function() {
+                    $(_t.$refs[_t.menu.tabMenuItems[d].page]).transition('fade up', '100ms')
+                }, 100)
+            }
         },
         getMusicName: function(p) {
             if (this.useExperienceCutName) {
@@ -142,7 +166,7 @@ let Vety = new Vue({
                 let vety_audio = $("#mainMusic").get('0')
                 $("#mainMusic").get('0').pause()
                 _t.status = 0
-                _t.nowTime = 0
+                _t.player.nowTime = 0
                 vety_audio.load()
             })
         },
@@ -206,13 +230,13 @@ let Vety = new Vue({
             this.chatWithPyQt("update " + JSON.stringify(this.config))
         },
         changePercent: function(v) {
-            this.playFrom(this.allTime / 100 * v)
+            this.playFrom(this.player.allTime / 100 * v)
         },
         nowPercent() {
-            if (this.nowTime == 0)
+            if (this.player.nowTime == 0)
                 return 0
             else
-                return this.nowTime / this.allTime * 100
+                return this.player.nowTime / this.player.allTime * 100
         },
         showContextMenu(q, e) {
             let _t = this
@@ -252,7 +276,7 @@ let Vety = new Vue({
                     let vety_audio = $("#mainMusic").get('0')
                     $("#mainMusic").get('0').pause()
                     _t.status = 0
-                    _t.nowTime = 0
+                    _t.player.nowTime = 0
                     vety_audio.load()
                     _t.play()
                 })
@@ -271,7 +295,7 @@ let Vety = new Vue({
                 let vety_audio = $("#mainMusic").get('0')
                 $("#mainMusic").get('0').pause()
                 _t.status = 0
-                _t.nowTime = 0
+                _t.player.nowTime = 0
                 vety_audio.load()
                 _t.play()
                 vety_audio.onended = () => {
